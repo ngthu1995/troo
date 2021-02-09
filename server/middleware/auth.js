7094914713ca424c8e12172579185e7505afbc98
ngthu1995 <ngthu1995@gmail.com>
2021-02-08 22:05:37 -0500

    uncomment the logic of auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,6 @@
 const { User } = require("./../models/user");
 
 let auth = (req, res, next) => {
-  return res.status(200).json({
-    isAuth: false,
-    error: true,
-  });
   let token = req.cookies.w_auth;
 
   User.findByToken(token, (err, user) => {
