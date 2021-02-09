8266fa5afbbe8fe2ecaf6127507b7a41aa099b37
ngthu1995 <ngthu1995@gmail.com>
2021-02-08 20:11:25 -0500

    test more data

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,19 @@
 const { User } = require("./../models/user");
 
 let auth = (req, res, next) => {
-  let token = req.cookies.w_auth;
-  console.log("🚀 ~ file: auth.js ~ line 5 ~ auth ~ token", token);
+  // let token = req.cookies.w_auth;
+  // console.log("🚀 ~ file: auth.js ~ line 5 ~ auth ~ token", token);
+  let token = "123";
 
   User.findByToken(token, (err, user) => {
-    // if (err) {
-    //   console.log("🚀 ~ file: auth.js ~ line 9 ~ User.findByToken ~ err", err);
-    //   return res.json({
-    //     isAuth: false,
-    //     error: true,
-    //   });
-    // }
-    if (err) throw err;
+    if (err) {
+      console.log("🚀 ~ file: auth.js ~ line 9 ~ User.findByToken ~ err", err);
+      return res.json({
+        isAuth: false,
+        error: true,
+      });
+    }
+    // if (err) throw err;
     if (!user)
       return res.json({
         isAuth: false,
